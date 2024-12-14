31a47fa7dc0a29d93a6167223c735c12450d1196
liangzemian <liangzemian@Gmail.com>
2024-12-14 17:13:31 +0800

    test

--- init-android.sh
+++ init-android.sh
@@ -17,9 +17,13 @@
 #
 
 # IJK_FFMPEG_UPSTREAM=git://git.videolan.org/ffmpeg.git
-IJK_FFMPEG_UPSTREAM=https://github.com/liangzemian/FFmpeg-webrtc.git
-IJK_FFMPEG_FORK=https://github.com/liangzemian/FFmpeg-webrtc.git
-IJK_FFMPEG_COMMIT=ijkplayer
+# IJK_FFMPEG_UPSTREAM=https://github.com/liangzemian/FFmpeg-webrtc.git
+# IJK_FFMPEG_FORK=https://github.com/liangzemian/FFmpeg-webrtc.git
+# IJK_FFMPEG_COMMIT=ijkplayer
+# IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
+IJK_FFMPEG_UPSTREAM=https://github.com/tonychanchen/FFmpeg.git
+IJK_FFMPEG_FORK=https://github.com/tonychanchen/FFmpeg.git
+IJK_FFMPEG_COMMIT=ff4.0--ijk0.8.8--20210205--003
 IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
 
 set -e
